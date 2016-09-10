a72e7fd24d4720df099c604999445a976c07a694
aleksZubakov <aleks.zubakov@gmail.com>
2016-09-11 00:01:19 +0300

    Added controller with parsing for start flash-course bot's

--- controller/flash_course.py
+++ controller/flash_course.py
@@ -1,34 +1,28 @@
-import sys
+"""
+    Flash-course bot runner by symmetrical-potato
+    with fucking threads :)
+"""
+
+import argparse
 
 import logging
 import asyncio
+import threading
+
 
 from telegram.ext import Updater, Handler, CommandHandler, MessageHandler, Filters
 from telegram.replykeyboardmarkup import ReplyKeyboardMarkup
 from telegram import KeyboardButton
 
-import model.Courses as md
-
-clients = dict() #?
+# from model.Courses import Courses
 
 
-token = '254385124:AAFzW49rQixpdUMfGpG3h2FYB049lDtBYJk'
-
-updater = Updater(token=token)
-dispatcher = updater.dispatcher
-
-
-md.init_bot(token)
-
-loop = asyncio.get_event_loop()
-
-
+""" Handlers """
 def on_start_command(bot, update):
     chat_id = update.message.chat_id
     #initialize number of message
-    clients[chat_id] = 0
-
-
+    clients[chat_id] = {'red_message': True,
+                        'timer' : 3}
     #first_message = md.new_user(token, chat_id)
 
     user_name = update.message.from_user['first_name'] + " " + update.message.from_user['last_name']
@@ -39,47 +33,78 @@ def on_start_command(bot, update):
 
     #bot.sendMessage(chat_id=chat_id, text=first_message)
 
-    run_loop(chat_id)
-
-
-
-def run_loop(chat_id):
-    #!check time
-    #loop.call_later(1000000, send_course_message, chat_id)
-    send_course_message(chat_id)
-
-
-
-def send_course_message(chat_id):
-
-    message = 'lorem ipsum'
-
-    kb = ReplyKeyboardMarkup([['Прочитал, вполне прикольно :)']], one_time_keyboard=True)
-    updater.bot.sendMessage(chat_id=chat_id, text=message, reply_markup=kb)
-
 def on_text_message(bot, update):
     chat_id = update.message.chat_id
-
     if update.message.text == 'Прочитал, вполне прикольно :)':
         bot.sendMessage(chat_id=chat_id, text='А ты хорош:)')
+        clients[chat_id]['red_message'] = True
 
 
+"""Wrappers"""
+def updater_wrapper():
+    updater.start_polling()
+
+def async_loop_wrapper():
+    global loop
+    loop = asyncio.new_event_loop()
+    try:
+        loop.run_until_complete(send_course_message())
+    finally:
+        loop.close()
 
 
+@asyncio.coroutine
+def send_course_message():
+    while True:
+        asyncio.set_event_loop(loop=loop)
+        if not clients: #check if empty
+            yield from asyncio.sleep(1)
+
+        for chat_id, client in clients.items():
+            if client['timer'] <= 0:
+                if client['red_message']:
+                    message = 'lorem ipsum'
+                    #timer, message =  model.get_info(token, chat_id)
+                    key_board = ReplyKeyboardMarkup([['Прочитал, вполне прикольно :)']], one_time_keyboard=True)
+                    updater.bot.sendMessage(chat_id=chat_id, text=message, reply_markup=key_board)
+
+                    client['red_message'] = False
+                    # model.set_read(token, chat_id)
+                    # client['timer'] =
+                else:
+                    continue
+            else:
+                client['timer'] -= 3
+        yield from asyncio.sleep(1)
 
 
-print("Running main loop")
-#loop.run_forever()
+if __name__ == '__main__':
+    #variables
+    clients = dict()
 
+    #token parsing
+    parser = argparse.ArgumentParser(description='Run flash-course telegram bot by your token')
+    parser.add_argument('token', help='Just add token;)',
+                        type=str)
+    token = parser.parse_args().token
 
+    #telegram updater
+    updater = Updater(token=token)
+    dispatcher = updater.dispatcher
 
-start_handler = CommandHandler('start', on_start_command)
-mesg_handler = MessageHandler([Filters.text], on_text_message)
+    #handlers
+    start_handler = CommandHandler('start', on_start_command)
+    mesg_handler = MessageHandler([Filters.text], on_text_message)
 
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(mesg_handler)
 
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(mesg_handler)
+    threads_targets = [updater_wrapper, async_loop_wrapper]
+    for target in threads_targets:
+        threading.Thread(target=target).start()
 
+    # database init
 
+    # model = Courses()
+    # model.init_bot(token)
 
-updater.start_polling()
